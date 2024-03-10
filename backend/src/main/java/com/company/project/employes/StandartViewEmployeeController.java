5e286d7f10634d56904a3f8258c77221ec60d2fc
Eduard Shapkin <snd009@mail.ru>
2024-03-10 16:25:27 +0300

    add api view controller

--- backend/src/main/java/com/company/project/employes/StandartViewEmployeeController.java
+++ backend/src/main/java/com/company/project/employes/StandartViewEmployeeController.java
@@ -4,10 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,9 +20,46 @@ public class StandartViewEmployeeController implements ViewEmployeeController {
     //        @Override
     @GetMapping("/")
     //200 - if true
-    public String showAllEmployees(String name, @ModelAttribute("model") ModelMap model) {
+    public String showAllEmployees(String name,
+                                   @ModelAttribute("model") ModelMap model) {
         ResponseEntity<List<Employee>> response = standartEmployeeController.getEmployees();
         model = model.addAttribute("employeeList", response.getBody());
         return "employees";
+    }
+
+    @GetMapping(value = {"/add"})
+    public String showAddEmployee(Model model) {
+        return "";
+    }
+
+    @PostMapping(value = "/add")
+    public String addEmployee(Model model,
+                              @ModelAttribute("employee") EmployeeDto employee) {
+        return "";
+    }
+
+    @GetMapping(value = {"/{employeeId}/edit"})
+    public String showEditEmployee(Model model,
+                                   @PathVariable long employeeId) {
+        return "";
+    }
+
+    @PostMapping(value = {"/{employeeId}/edit"})
+    public String updateEmployee(Model model,
+                             @PathVariable long employeeId,
+                             @ModelAttribute("employee") EmployeeDto employee) {
+        return "";
+    }
+
+    @GetMapping(value = {"/{employeeId}/delete"})
+    public String showDeleteEmployeeById(Model model,
+                                         @PathVariable long employeeId) {
+        return "";
+    }
+
+    @PostMapping(value = {"/{employeeId}/delete"})
+    public String deleteEmployeeById(Model model,
+                                     @PathVariable long employeeId) {
+        return "";
     }
 }
