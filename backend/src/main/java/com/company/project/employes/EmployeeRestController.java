c05a3751d8bf66ca889f676cc396e20e6e16e92b
Eduard Shapkin <snd009@mail.ru>
2024-03-20 02:28:32 +0300

    add layer EmployeeViewController as EmployeeRestController

---
+++ backend/src/main/java/com/company/project/employes/EmployeeRestController.java
@@ -0,0 +1,64 @@
+package com.company.project.employes;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api/v2/employees")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class EmployeeRestController implements ViewEmployeeController {
+    private final ViewEmployeeController exceptionCatchedViewEmployeeController;
+    @Override
+    @GetMapping("/all")
+    public String showAllEmployees(@ModelAttribute("model") ModelMap model) {
+        return exceptionCatchedViewEmployeeController.showAllEmployees(model);
+    }
+
+    @Override
+    @GetMapping(value = "/add")
+    public String showAddEmployee(Model model) {
+        return exceptionCatchedViewEmployeeController.showAddEmployee(model);
+    }
+
+    @Override
+    @PostMapping(value = "/add")
+    public String addEmployee(Model model,  @ModelAttribute("employee") EmployeeDto employee) {
+        return exceptionCatchedViewEmployeeController.addEmployee(model, employee);
+    }
+
+    @Override
+    @GetMapping(value = {"/{employeeId}/edit"})
+    public String showEditEmployee(Model model, @PathVariable long employeeId) {
+        return exceptionCatchedViewEmployeeController.showEditEmployee(model, employeeId);
+    }
+
+    @Override
+    @PostMapping(value = {"/{employeeId}/edit"})
+    public String updateEmployee(Model model,
+                                 @PathVariable Integer employeeId,
+                                 @ModelAttribute("employee") EmployeeDto employee) {
+        return exceptionCatchedViewEmployeeController.updateEmployee(model, employeeId, employee);
+    }
+
+    @Override
+    @GetMapping(value = {"/{employeeId}/delete"})
+    public String showDeleteEmployeeById(@ModelAttribute("model") ModelMap model,
+                                         @PathVariable long employeeId) {
+        return exceptionCatchedViewEmployeeController.showDeleteEmployeeById(model, employeeId);
+    }
+
+    @Override
+    public String deleteEmployeeById(ModelMap model, Integer employeeId) {
+        return exceptionCatchedViewEmployeeController.deleteEmployeeById(model, employeeId);
+    }
+
+    @Override
+    @GetMapping(value = "/{employeeId}/show")
+    public String getNoteById(Model model, @PathVariable Integer employeeId) {
+        return exceptionCatchedViewEmployeeController.getNoteById(model, employeeId);
+    }
+}
--- backend/src/main/java/com/company/project/employes/ExceptionCatchedViewEmployeeController.java
+++ backend/src/main/java/com/company/project/employes/ExceptionCatchedViewEmployeeController.java
@@ -10,26 +10,22 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
-@RequestMapping("/api/v2/employees")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ExceptionCatchedViewEmployeeController implements ViewEmployeeController {
     private final ViewEmployeeController standartViewEmployeeController;
 
     @Override
-    @GetMapping("/all")
-    public String showAllEmployees(@ModelAttribute("model") ModelMap model) {
+    public String showAllEmployees(ModelMap model) {
         return standartViewEmployeeController.showAllEmployees(model);
     }
 
     @Override
-    @GetMapping(value = "/add")
     public String showAddEmployee(Model model) {
         return standartViewEmployeeController.showAddEmployee(model);
     }
 
     @Override
-    @PostMapping(value = "/add")
-    public String addEmployee(Model model, @ModelAttribute("employee") EmployeeDto employee) {
+    public String addEmployee(Model model, EmployeeDto employee) {
         try {
             return standartViewEmployeeController.addEmployee(model, employee);
         } catch (Exception exception) {
@@ -42,12 +38,9 @@ public class ExceptionCatchedViewEmployeeController implements ViewEmployeeContr
     }
 
     @Override
-    @GetMapping(value = {"/{employeeId}/edit"})
-    public String showEditEmployee(Model model, @PathVariable long employeeId) {
+    public String showEditEmployee(Model model, long employeeId) {
         EmployeeDto employee = null;
         try {
-//            ResponseEntity<List<Employee>> response = exceptionCatchedViewEmployeeController.getEmployees((int) employeeId);
-//            employee = new EmployeeMapper(response.getBody().get(0)).mapToDto();
             return standartViewEmployeeController.showEditEmployee(model, employeeId);
         } catch (Exception ex) {
             model.addAttribute("errorMessage", ex.getMessage());
@@ -58,16 +51,10 @@ public class ExceptionCatchedViewEmployeeController implements ViewEmployeeContr
     }
 
     @Override
-    @PostMapping(value = {"/{employeeId}/edit"})
     public String updateEmployee(Model model,
-                                 @PathVariable Integer employeeId,
-                                 @ModelAttribute("employee") EmployeeDto employee) {
+                                 Integer employeeId,
+                                 EmployeeDto employee) {
         try {
-//            employee.setId(employeeId);
-//            exceptionCatchedViewEmployeeController.putEmployee(employee);
-//            ResponseEntity<List<Employee>> response = exceptionCatchedViewEmployeeController.getEmployees();
-//            model = model.addAttribute("employeeList", response.getBody());
-//            return "redirect:/api/v2/employees/" + employee.getId() + "/show";
             return standartViewEmployeeController.updateEmployee(model, employeeId, employee);
         } catch (Exception ex) {
             String errorMessage = ex.getMessage();
@@ -79,9 +66,8 @@ public class ExceptionCatchedViewEmployeeController implements ViewEmployeeContr
     }
 
     @Override
-    @GetMapping(value = {"/{employeeId}/delete"})
-    public String showDeleteEmployeeById(@ModelAttribute("model") ModelMap model,
-                                         @PathVariable long employeeId) {
+    public String showDeleteEmployeeById(ModelMap model,
+                                         long employeeId) {
         return standartViewEmployeeController.showDeleteEmployeeById(model, employeeId);
     }
 
@@ -91,8 +77,7 @@ public class ExceptionCatchedViewEmployeeController implements ViewEmployeeContr
     }
 
     @Override
-    @GetMapping(value = "/{employeeId}/show")
-    public String getNoteById(Model model, @PathVariable Integer employeeId) {
+    public String getNoteById(Model model, Integer employeeId) {
         try {
             standartViewEmployeeController.getNoteById(model, employeeId);
         } catch (Exception ex) {
